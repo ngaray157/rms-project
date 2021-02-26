88ad298052569d50d6f9399960bed511f1f37c3f
Nicole <ngaray157@west-mec.org>
2021-02-26 14:01:11 -0700

    Routing for vehicles and people

---
+++ controllers/peopleController.js
@@ -0,0 +1,39 @@
+// Display list of all people.
+exports.people_list = function (req, res) {
+    res.send('NOT IMPLEMENTED: people list');
+};
+
+// Display detail page for a specific person.
+exports.people_detail = function (req, res) {
+    res.send('NOT IMPLEMENTED: people detail: ' + req.params.id);
+};
+
+// Display person create form on GET.
+exports.people_create_get = function (req, res) {
+    res.send('NOT IMPLEMENTED: people create GET');
+};
+
+// Handle person create on POST.
+exports.people_create_post = function (req, res) {
+    res.send('NOT IMPLEMENTED: people create POST');
+};
+
+// Display person delete form on GET.
+exports.people_delete_get = function (req, res) {
+    res.send('NOT IMPLEMENTED: people delete GET');
+};
+
+// Handle person delete on POST.
+exports.people_delete_post = function (req, res) {
+    res.send('NOT IMPLEMENTED: people delete POST');
+};
+
+// Display person update form on GET.
+exports.people_update_get = function (req, res) {
+    res.send('NOT IMPLEMENTED: people update GET');
+};
+
+// Handle person update on POST.
+exports.people_update_post = function (req, res) {
+    res.send('NOT IMPLEMENTED: people update POST');
+};
---
+++ controllers/vehicleController.js
@@ -0,0 +1,39 @@
+// Display list of all vehicle.
+exports.vehicle_list = function (req, res) {
+    res.send('NOT IMPLEMENTED: vehicle list');
+};
+
+// Display detail page for a specific vehicle.
+exports.vehicle_detail = function (req, res) {
+    res.send('NOT IMPLEMENTED: vehicle detail: ' + req.params.id);
+};
+
+// Display vehicle create form on GET.
+exports.vehicle_create_get = function (req, res) {
+    res.send('NOT IMPLEMENTED: vehicle create GET');
+};
+
+// Handle vehicle create on POST.
+exports.vehicle_create_post = function (req, res) {
+    res.send('NOT IMPLEMENTED: vehicle create POST');
+};
+
+// Display vehicle delete form on GET.
+exports.vehicle_delete_get = function (req, res) {
+    res.send('NOT IMPLEMENTED: vehicle delete GET');
+};
+
+// Handle vehicle delete on POST.
+exports.vehicle_delete_post = function (req, res) {
+    res.send('NOT IMPLEMENTED: vehicle delete POST');
+};
+
+// Display vehicle update form on GET.
+exports.vehicle_update_get = function (req, res) {
+    res.send('NOT IMPLEMENTED: vehicle update GET');
+};
+
+// Handle vehicle update on POST.
+exports.vehicle_update_post = function (req, res) {
+    res.send('NOT IMPLEMENTED: vehicle update POST');
+};
